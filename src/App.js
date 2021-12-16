acbf0a9834c08f8bc4a9aec05515f18f7b0ed2e6
Abolfazl <abolfazlm181920@gmail.com>
2021-12-16 22:10:34 +0330

    import useState to use show Square component1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Board from './Board';
 import Square from './Square';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [squares, setSquares] = useState();
+  
   return (
     <main>
       <Board>
