983bbdbfbfaee7214e1a291b7d20b4654fa9fc03
Abolfazl <abolfazlm181920@gmail.com>
2021-12-16 22:50:41 +0330

    add changes!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,103 @@
 import './App.css';
-import Board from './Board';
-import Square from './Square';
-import { useState } from 'react';
+import Board from "./Board";
+import Square from "./Square";
+import {useState, useEffect} from 'react';
 
 const defaultSquares = () => (new Array(9)).fill(null);
 
-const App = () => {
+const lines = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6],
+];
 
+function App() {
   const [squares, setSquares] = useState(defaultSquares());
+  const [winner,setWinner] = useState(null);
 
-  const handleSquareClick = () => {
-    let newSquares = squares;
-    newSquares[index] = 'x';
-    setSquares([...newSquares]);
+  useEffect(() => {
+    const isComputerTurn = squares.filter(square => square !== null).length % 2 === 1;
+    const linesThatAre = (a,b,c) => {
+      return lines.filter(squareIndexes => {
+        const squareValues = squareIndexes.map(index => squares[index]);
+        return JSON.stringify([a,b,c].sort()) === JSON.stringify(squareValues.sort());
+      });
+    };
+    const emptyIndexes = squares
+      .map((square,index) => square === null ? index : null)
+      .filter(val => val !== null);
+    const playerWon = linesThatAre('x', 'x', 'x').length > 0;
+    const computerWon = linesThatAre('o', 'o', 'o').length > 0;
+    if (playerWon) {
+      setWinner('x');
+    }
+    if (computerWon) {
+      setWinner('o');
+    }
+    const putComputerAt = index => {
+      let newSquares = squares;
+      newSquares[index] = 'o';
+      setSquares([...newSquares]);
+    };
+    if (isComputerTurn) {
+
+      const winingLines = linesThatAre('o', 'o', null);
+      if (winingLines.length > 0) {
+        const winIndex = winingLines[0].filter(index => squares[index] === null)[0];
+        putComputerAt(winIndex);
+        return;
+      }
+
+      const linesToBlock = linesThatAre('x', 'x', null);
+      if (linesToBlock.length > 0) {
+        const blockIndex = linesToBlock[0].filter(index => squares[index] === null)[0];
+        putComputerAt(blockIndex);
+        return;
+      }
+
+      const linesToContinue = linesThatAre('o', null, null);
+      if (linesToContinue.length > 0) {
+        putComputerAt(linesToContinue[0].filter(index => squares[index] === null)[0]);
+        return;
+      }
+
+      const randomIndex = emptyIndexes[ Math.ceil(Math.random()*emptyIndexes.length) ];
+      putComputerAt(randomIndex);
+    }
+  }, [squares]);
+
+
+
+  function handleSquareClick(index) {
+    const isPlayerTurn = squares.filter(square => square !== null).length % 2 === 0;
+    if (isPlayerTurn) {
+      let newSquares = squares;
+      newSquares[index] = 'x';
+      setSquares([...newSquares]);
+    }
   }
 
   return (
     <main>
       <Board>
-        {
-          squares.map((square,index) =>
+        {squares.map((square,index) =>
           <Square
-            x = {Square === 'x' ? 1 : 0}
-            o = {Square === 'o' ? 1 : 0}
-            onClick={() => handleSquareClick(index)} />)
-        }
+            x={square==='x'?1:0}
+            o={square==='o'?1:0}
+            onClick={() => handleSquareClick(index)} />
+        )}
       </Board>
+      {!!winner && winner === 'x' && (
+        <div className="result green">
+          You WON!
+        </div>
+      )}
+      {!!winner && winner === 'o' && (
+        <div className="result red">
+          You LOST!
+        </div>
+      )}
+
     </main>
   );
 }
